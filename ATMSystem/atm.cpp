4ea48d6da52113bc5f03e8caff6489336fd078f4
Tabot Chares Bessong <ebezebeatrice@gmail.com>
2021-12-21 02:48:54 +0100

    atm machine

---
+++ ATMSystem/atm.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+using namespace std;
+
+void showMenu (){
+  cout<<"**************Menu***********"<<endl;
+  cout<<"1 Check Balance"<<endl;
+  cout<<"2 Deposit"<<endl;
+  cout<<"3 withdraw"<<endl;
+    cout<<"**************((())))***********"<<endl;
+}
+
+int main (){
+    // show menu 
+  showMenu();
+  // get options from user 
+  int option;
+  double balance = 500;
+  
+  cout <<"Option: ";
+  cin >>option ;
+  switch(option){
+    // check balance 
+    case 1: 
+      cout<<"Balance is: "<<"$"<<balance<<endl;
+      break;
+    // deposit 
+    case 2:
+      cout<<"Deposit amount: ";
+      double depositAmount ;
+      cout <<"Enter amount you wish to deposit"<<endl;
+      cin>> depositAmount;
+      balance += depositAmount;break ;
+
+      case 3:
+        cout<<"Withdrawal amount: ";
+        double withdrawAmount ;
+        // cout <<"Enter amount you wish to deposit"<<endl;
+        cin>> withdrawAmount;
+        if(withdrawAmount < balance)
+          balance -= withdrawAmount;break ;
+        else
+          cout<<"Not enough fund";
+  }
+
+  
+
+  // withdraw 
+
+
+  return 0;
+}
---
+++ SEARCHINGALGO/atm.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+void showMenu (){
+  cout<<"**************Menu***********"<<endl;
+  cout<<"1 Check Balance"<<endl;
+  cout<<"1 Check Balance"<<endl;
+  cout<<"1 Check Balance"<<endl;
+    cout<<"**************((())))***********"<<endl;
+}
+
+int main (){
+
+  // get options from user 
+  int option;
+  cout <<"Option: ";
+  cin >>option ;
+  // show menu 
+  showMenu();
+
+  // check balance 
+
+  // deposit 
+
+  // withdraw 
+
+
+  return 0;
+}
