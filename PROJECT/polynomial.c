97ed6cd64681cf2da507a7335eeb6c2cd6e19a89
Charles Bessong Tabot <ebezebeatrice@gmail.com>
2022-01-09 18:43:41 +0100

    added a create and print polynomial function

--- PROJECT/polynomial.c
+++ PROJECT/polynomial.c
@@ -1,6 +1,7 @@
 
 
 #include<stdio.h>
+#include<stdlib.h>
 
 
 // the structure above is a structure that enables us to create our node for each values of the polynomial 
@@ -14,28 +15,7 @@ struct node {
 // we are going to use sorted linked  list 
 // we will need an ordered linked list so we can do correct arithmetic operation 
 
-struct node* create(struct node* head){
-  int n; 
-  int i;
-  float coef;
-  int expo;
-  printf("Enter the number of terms: ");
-  scanf("%d",&n);
-  printf("Please enter the coefficient: ");
-  scanf("%d",&i);
-  // printf(" The value is %d",n);
-  // int c = n * 3 ;
-  // printf("%d",c);
-  for(i = 0;i<n;i++){
-    printf("Enter the coefficient for term %d: ", i+1);
-    scanf("%f",&coef);
-    printf("Enter the exponent for term %d: ", i+1);
-    scanf("%f",&expo);
-    head = insert(head,coef,expo);
-  }
-}
-
-struct node* insert(struct node* head , int co , int ex){
+struct node* insert(struct node* head , float co , int ex){
   struct node* temp;
   struct node* newP = malloc(sizeof(struct node));
   newP->coef = co ;
@@ -54,12 +34,56 @@ struct node* insert(struct node* head , int co , int ex){
   return head;
 }
 
+struct node* create(struct node* head){
+  int n; 
+  int i;
+  float coef;
+  int expo;
+  printf("Enter the number of terms: ");
+  scanf("%d",&n);
+  // printf("Please enter the coefficient: ");
+  // scanf("%d",&i);
+  // printf(" The value is %d",n);
+  // int c = n * 3 ; 
+  // printf("%d",c);
+  for(i = 0;i<n;i++){
+    printf("Enter the coefficient for term %d: ", i+1);
+    scanf("%f",&coef);
+    printf("Enter the exponent for term %d: ", i+1);
+    scanf("%f",&expo);
+    head = insert(head,coef,expo);
+  }
+}
+
+
+
+// function to print our polynomial 
+void print(struct node* head){
+  if(head == NULL)
+    printf("No polunomial available");
+  else {
+    struct node* temp = head;
+    while(temp != NULL){
+      printf("(%.1fx^%d)",temp->coef , temp->expo);
+      temp = temp->link;
+      if(temp != NULL)
+        printf(" + ");
+      else
+        printf("\n");
+    }
+  }
+}
+
 int main (){
   // printf("Hello world");
   // first step declare a node in the main function 
   struct node* head = NULL;
   printf("Enter the polynomials:\n ");
   head = create(head);
+
+  // printing the polynomial 
+  print(head);
+  return 0;
 }
 
 // we are going  to have a seperate function to create a node for each polynomial value 
