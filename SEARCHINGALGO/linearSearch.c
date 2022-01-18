cd5b0847b12775753f1465943dfdc88329c65f97
Charles Bessong Tabot <ebezebeatrice@gmail.com>
2022-01-18 14:53:10 +0100

    modified  linearSeahc.c

--- SEARCHINGALGO/linearSearch.c
+++ SEARCHINGALGO/linearSearch.c
@@ -1,21 +1,29 @@
 #include<stdio.h>
 
+// in c , we sometimes or calways aneed to call the function before the main function 
 int linearSearch(int A[] , int n , int value);
+	int arr[] = {2,6,1,4,-3,5,4};
+
 
 int main(void){
-	int arr[6] = {2,6,1,-3,5,4};
-	int index = linearSearch(arr,6,4);
-	printf("\n%d",index);
+	int n = sizeof(arr) / sizeof(arr[0]);
+	int index = linearSearch(arr,n,4);
+	printf("The  number you are looking for is at index %d",index);
 	
 	return 0;
 }
 
 int linearSearch(int A[], int n , int value){
 	int i ;
+	// so we are simply iterating between the the values until we meet  one that is same  as  our  value we are  looking for 
 	for(i=0;i<n;i++){
-		if(A[i] == value)
+		if(A[i] == value){
+			int j = i;
 			return i;
-	
+			while(i < n){
+				linearSearch(arr,n,4);
+			}
+		}
 	}
 	return -1;
 }
