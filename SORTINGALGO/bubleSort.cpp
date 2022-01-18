61442c8807d0cbf4470ef45d01ca7f4cfe1b5494
Charles Bessong Tabot <ebezebeatrice@gmail.com>
2022-01-18 13:12:03 +0100

    adding comment to the bubbke sort.cpp for better understanding

--- SORTINGALGO/bubleSort.cpp
+++ SORTINGALGO/bubleSort.cpp
@@ -3,13 +3,17 @@
  
  void bubbleSortMin(int B[] , int n){
  	for(int k = 0;k<n-1;k++){
+		 // with this algorithm , we will alaways start at the begining  of the array 
  		int flag = 0;
  		for (int i = 0;i< n-1;i++){
+			 // this loop we will iterate and swap the vaues  to arrange them depending on if we want it to be  from lowest to highest or vice-versa 
  			if(B[i] > B[i+1]){
  				swap(B[i] , B[i+1]);
+				 // this value of flag = 1 gives us the permission to swap the values 
  				flag = 1;
 			 }
 		 }
+		 // when ever  we reach a values of 0 we stop telling us that we need to stop the iteration 
 		 if(flag == 0)
 		 	break;
 	 }
