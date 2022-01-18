60120be1837c80820fac13b9947d4b5d78a73c79
Charles Bessong Tabot <ebezebeatrice@gmail.com>
2022-01-18 16:51:57 +0100

    updating the binary search

---
+++ SEARCHINGALGO/binSearch.js
@@ -0,0 +1,5 @@
+
+
+const values  = [0,1,2,3,4,5,6,7,8,9,10]
+
+const binary = (values,arr)
--- SEARCHINGALGO/binarySearch.cpp
+++ SEARCHINGALGO/binarySearch.cpp
@@ -6,10 +6,13 @@ using namespace std;
 
 int binSearch(int arr[] , int l, int r , int x){
 	if(r >= 1){
-		int mid = 1 + (r - 1) /2;
+		// if we  have  one or  more element  , we will have to devide and begin to search to either the left side or right side of the array 
+		int mid = l + (r - 1) /2;
 		if(arr[mid] == x)
+		// since we are dividing the array into 2 , if the value is at the middle  , we  return that middle index
 			return mid;
 		if(arr[mid] > x)
+		// to make the search easy for me , i devided the array into 2  , so if the value i am looking for is less than that at the  middle i will seach to the  left and  vice-versa
 			return binSearch(arr,l,mid-1,x);
 		return binSearch(arr,mid+1,r,x);
 	}	
@@ -22,7 +25,8 @@ int main(){
 	
 //	cout <<"Enter your number";
 //	cin >>x;
+  cout << "Hello world"<<endl;
 	int n= sizeof(arr)/sizeof(arr[0]);
 	int result = binSearch(arr,0,n-1,x);
-	cout <<"The element "<<x<<"is at index "<<result;
+	cout <<"The element "<<x <<" is at index "<< result;
 }
