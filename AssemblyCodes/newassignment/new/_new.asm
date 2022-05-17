ef0724e204557b6e4eb843f9827bbbbf53315ce8
Tabot Charles Bessong <ebezebeatrice@gmail.com>
2022-05-17 12:43:18 +0100

    updated counter code

---
+++ AssemblyCodes/newassignment/new/_new.asm
@@ -0,0 +1,108 @@
+
+_main:
+
+;_new.c,1 :: 		void main() {
+;_new.c,3 :: 		int ssDev[10] = {0X7E, 0X06, 0X6D, 0X79, 0X33, 0X5B, 0X5F, 0X70, 0X7F, 0X7B};
+	MOVLW      126
+	MOVWF      main_ssDev_L0+0
+	MOVLW      0
+	MOVWF      main_ssDev_L0+1
+	MOVLW      6
+	MOVWF      main_ssDev_L0+2
+	MOVLW      0
+	MOVWF      main_ssDev_L0+3
+	MOVLW      109
+	MOVWF      main_ssDev_L0+4
+	MOVLW      0
+	MOVWF      main_ssDev_L0+5
+	MOVLW      121
+	MOVWF      main_ssDev_L0+6
+	MOVLW      0
+	MOVWF      main_ssDev_L0+7
+	MOVLW      51
+	MOVWF      main_ssDev_L0+8
+	MOVLW      0
+	MOVWF      main_ssDev_L0+9
+	MOVLW      91
+	MOVWF      main_ssDev_L0+10
+	MOVLW      0
+	MOVWF      main_ssDev_L0+11
+	MOVLW      95
+	MOVWF      main_ssDev_L0+12
+	MOVLW      0
+	MOVWF      main_ssDev_L0+13
+	MOVLW      112
+	MOVWF      main_ssDev_L0+14
+	MOVLW      0
+	MOVWF      main_ssDev_L0+15
+	MOVLW      127
+	MOVWF      main_ssDev_L0+16
+	MOVLW      0
+	MOVWF      main_ssDev_L0+17
+	MOVLW      123
+	MOVWF      main_ssDev_L0+18
+	MOVLW      0
+	MOVWF      main_ssDev_L0+19
+;_new.c,4 :: 		TRISB = 0X00;
+	CLRF       TRISB+0
+;_new.c,5 :: 		PORTB = 0X00;
+	CLRF       PORTB+0
+;_new.c,6 :: 		while(1){
+L_main0:
+;_new.c,8 :: 		i =0;
+	CLRF       R3+0
+	CLRF       R3+1
+;_new.c,9 :: 		while(i < 10){
+L_main2:
+	MOVLW      128
+	XORWF      R3+1, 0
+	MOVWF      R0+0
+	MOVLW      128
+	SUBWF      R0+0, 0
+	BTFSS      STATUS+0, 2
+	GOTO       L__main6
+	MOVLW      10
+	SUBWF      R3+0, 0
+L__main6:
+	BTFSC      STATUS+0, 0
+	GOTO       L_main3
+;_new.c,10 :: 		PORTB = ssDev[i];
+	MOVF       R3+0, 0
+	MOVWF      R0+0
+	MOVF       R3+1, 0
+	MOVWF      R0+1
+	RLF        R0+0, 1
+	RLF        R0+1, 1
+	BCF        R0+0, 0
+	MOVF       R0+0, 0
+	ADDLW      main_ssDev_L0+0
+	MOVWF      FSR
+	MOVF       INDF+0, 0
+	MOVWF      PORTB+0
+;_new.c,11 :: 		Delay_ms(10000);
+	MOVLW      102
+	MOVWF      R11+0
+	MOVLW      118
+	MOVWF      R12+0
+	MOVLW      193
+	MOVWF      R13+0
+L_main4:
+	DECFSZ     R13+0, 1
+	GOTO       L_main4
+	DECFSZ     R12+0, 1
+	GOTO       L_main4
+	DECFSZ     R11+0, 1
+	GOTO       L_main4
+;_new.c,12 :: 		i++;
+	INCF       R3+0, 1
+	BTFSC      STATUS+0, 2
+	INCF       R3+1, 1
+;_new.c,13 :: 		}
+	GOTO       L_main2
+L_main3:
+;_new.c,14 :: 		}
+	GOTO       L_main0
+;_new.c,15 :: 		}
+L_end_main:
+	GOTO       $+0
+; end of _main
---
+++ AssemblyCodes/newassignment/new/last.cp
@@ -0,0 +1,16 @@
+#line 1 "C:/Users/CHARLES/Documents/ALGORITHMNOTES/AssemblyCodes/newassignment/new/last.c"
+void main() {
+ int i;
+ int ssDev[10] = {0X7E, 0X06, 0X6D, 0X79, 0X33, 0X5B, 0X5F, 0X70, 0X7F, 0X7B};
+ TRISB = 0X00;
+ PORTB = 0X00;
+ while(1){
+
+ int i =0;
+ while(i < 10){
+ PORTB = ssDev[i];
+ Delay_ms(1000);
+ i++;
+ }
+}
+}
